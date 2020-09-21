125c5a4eefdb321acda0564fbd191d7baba522cc
JayWang-huangliang-02761 <jay_wang@huangliang>
2020-09-21 14:48:25 +0800

    add test JS

---
+++ js/testJS.js
@@ -0,0 +1,17 @@
+var _video = document.querySelector('video');
+
+function isScrolledIntoView( element ) {
+    var elementTop    = element.getBoundingClientRect().top,
+        elementBottom = element.getBoundingClientRect().bottom;
+
+    return elementTop >= 0 && elementBottom <= window.innerHeight;
+}
+
+window.addEventListener("scroll", function(){
+  if (isScrolledIntoView(_video)) {
+    _video.play();
+  }
+  else {
+    _video.pause()
+  }
+})
